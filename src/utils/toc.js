91f2fd12eca4874a027650502cc47bab26a4d606
Jerome Vial <jerome.vial@gmail.com>
2024-06-11 15:16:22 +0200

    Enhance TOC

--- src/utils/toc.js
+++ src/utils/toc.js
@@ -1,27 +1,39 @@
 /**
- * Generates a Table of Contents (TOC) for a PDF document based on a file tree structure.
- *
- * @param {object} doc - The PDF document instance to add the TOC to.
- * @param {object} fileTree - The hierarchical structure representing files and directories.
- * @param {number} [depth=0] - The current depth in the file tree, used for indentation.
- * @param {string} [numbering=''] - The current numbering prefix for directory entries.
+ * Generates a Table of Contents (TOC) for the given file structure.
+ * @param {object} doc - The PDF document instance.
+ * @param {object} fileTree - The file structure object.
+ * @param {number} depth - The current depth in the directory structure (used for indentation).
+ * @param {string} numbering - The current numbering prefix.
  */
 const generateTOC = (doc, fileTree, depth = 0, numbering = '') => {
-  const indent = '  '.repeat(depth) // Create an indentation string based on the current depth
-  let index = 1
+  const indent = '  '.repeat(depth)
+  const files = []
+  const folders = []
+  let folderIndex = 1
 
+  // Separate files and folders
   Object.keys(fileTree).forEach(key => {
-    if (typeof fileTree[key] === 'object' && !fileTree[key].path) {
-      // It's a directory
-      const currentNumbering = numbering ? `${numbering}.${index}` : `${index}`
-      doc.fontSize(14).text(`${indent}${currentNumbering} ${key}/`, { destination: `${currentNumbering} ${key}/` })
-      generateTOC(doc, fileTree[key], depth + 1, currentNumbering) // Recursive call for directory contents
-      index++
-    } else if (fileTree[key].path) {
-      // It's a file
-      doc.fontSize(12).text(`${indent}- ${key}`, { continued: true }) // Indent and add file name with a bullet point
-      doc.text(` (${fileTree[key].size} KB)`, { align: 'right', destination: `${key}` }) // Add file size and link
+    if (typeof fileTree[key].path === 'string') {
+      files.push(key)
+    } else {
+      folders.push(key)
     }
+  })
+
+  // Prepare file entries for a single line display
+  const fileEntries = files.map(key => `${key} (${fileTree[key].size} KB)`).join(', ')
+
+  // Display files first without numbering, on a single line
+  if (fileEntries) {
+    doc.fontSize(9).text(`${indent} ${fileEntries}`)
+  }
+
+  // Display folders with numbering
+  folders.forEach(key => {
+    const currentNumbering = numbering ? `${numbering}.${folderIndex}` : `${folderIndex}`
+    doc.fontSize(14).text(`${indent}${currentNumbering} ${key}/`, { destination: `${currentNumbering} ${key}/` })
+    generateTOC(doc, fileTree[key], depth + 1, currentNumbering)
+    folderIndex++
   })
 }
 
